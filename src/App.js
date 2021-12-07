4b379de71cf2fcf694d21c5a3b73ed527969e725
David <davidgimeno1992@gmail.com>
2021-12-07 18:24:20 +0100

    feat: initial project push

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import MyImage from "./MyImage";
+
+function App() {
+  return (
+    <div>
+      <MyImage />
+      <p className="two-columns">
+        Una estrella cau al prat, una flor s’ha esbadellat, tot belant juga el
+        ramat amb la rossa macaruia. 
+        <br /> 
+        <br /> 
+        Al·leluia, cor lassat! Al·leluia, món
+        gebrat! Al·leluia, Déu és nat! Al·leluia!
+        <br />
+        <br />
+        Cap herbei té tenebror, ni cap
+        déu fa el ploricó; no hi ha fred ni tremolor, que un pas d’ala se n’ho
+        duia. 
+        <br />
+        <br />
+        Al·leluia en tot racó! Al·leluia en tot dolor! Al·leluia al
+        pecador! Al·leluia! 
+        <br />
+        <br />
+        A Betlem van els infants i els amics dant-se les
+        mans i els promesos i els germans i la vella en sa capuia. 
+        <br />
+        <br />
+        Al·leluia,
+        vianants! Al·leluia en nostres cants! Al·leluia, catalans! Al·leluia
+        {/* <br />
+        <br />
+        Josep Carner */}
+      </p>
+    </div>
+  );
+}
+
+export default App;
